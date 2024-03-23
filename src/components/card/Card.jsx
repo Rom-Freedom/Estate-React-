88f986a32d176b83d9d7ce21aa084057437d0170
Rom-Freedom <romeo.dark@mail.ru>
2024-03-24 00:08:21 +0300

    make up and style a ListPage component

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -9,8 +9,33 @@ function Card({ item }) {
             </Link>
             <div className='textContainer'>
                 <h2 className='title'>
-
+                    <Link to={`/${item.id}`}>{item.title}</Link>
                 </h2>
+                <p className="address">
+                    <img src="/pin.png" alt="" />
+                    <span>{item.address}</span>
+                </p>
+                <p className="price">$ {item.price}</p>
+                <div className="bottom">
+                    <div className="features">
+                        <div className="feature">
+                            <img src="/bed.png" alt="bed" />
+                            <span>{item.bedroom} bedroom</span>
+                        </div>
+                        <div className="feature">
+                            <img src="/bath.png" alt="bathroom" />
+                            <span>{item.bathroom} bathroom</span>
+                        </div>
+                    </div>
+                    <div className="icons">
+                        <div className="icon">
+                            <img src="/save.png" alt="icon" />
+                        </div>
+                        <div className="icon">
+                            <img src="/chat.png" alt="chat" />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div> 
     );
